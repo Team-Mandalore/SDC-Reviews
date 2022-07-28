d02e17b2d79e6c54fabff1a6e6da435d21640dc6
vpatel89 <virenpatel89@gmail.com>
2022-07-28 18:02:21 -0400

    all tables in database

--- server/schema.sql
+++ server/schema.sql
@@ -5,13 +5,10 @@ CREATE DATABASE reviewsservice;
 
 CREATE TABLE
   reviews(
-    -- userID int AUTO_INCREMENT,
-    -- username varchar(255) NOT NULL,
-    -- PRIMARY KEY (userID),
     id SERIAL PRIMARY KEY NOT NULL,
     product_id SERIAL NOT NULL,
     rating smallint,
-    -- date TIMESTAMPTZ NOT NULL,
+    date bigint NOT NULL,
     summary TEXT,
     body TEXT,
     recommend boolean,
@@ -22,22 +19,42 @@ CREATE TABLE
     helpfulness INTEGER
 );
 
-
-\copy reviews FROM 'server/data/reviewsTest.csv' DELIMITER ',' CSV HEADER;
-
--- CREATE TABLE
---   messages(
---     message_id int AUTO_INCREMENT,
---     text varchar(255) NOT NULL,
---     userID int NOT NULL,
---     roomID int NOT NULL,
---     PRIMARY KEY (message_id),
---     FOREIGN KEY (userID) REFERENCES users(userID),
---     FOREIGN KEY (roomID) REFERENCES rooms(roomID)
--- );
+\copy reviews FROM 'server/data/reviews.csv' DELIMITER ',' CSV HEADER;
 
 
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+CREATE TABLE
+  review_photos(
+    id SERIAL PRIMARY KEY NOT NULL,
+    review_id SERIAL NOT NULL REFERENCES reviews (id),
+    url TEXT
+);
+
+\copy review_photos FROM 'server/data/reviews_photos.csv' DELIMITER ',' CSV HEADER;
+
+
+
+CREATE TABLE
+  characteristics(
+    id SERIAL PRIMARY KEY NOT NULL,
+    product_id SERIAL NOT NULL,
+    name VARCHAR(255)
+);
+
+\copy characteristics FROM 'server/data/characteristics.csv' DELIMITER ',' CSV HEADER;
+
+
+
+CREATE TABLE
+  characteristic_reviews(
+    id SERIAL PRIMARY KEY NOT NULL,
+    characteristic_id SERIAL NOT NULL REFERENCES characteristics (id),
+    review_id SERIAL NOT NULL REFERENCES reviews (id),
+    value NUMERIC
+);
+
+\copy characteristic_reviews FROM 'server/data/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
+
+
+
+-- sudo -u postgres psql < server/schema.sql
